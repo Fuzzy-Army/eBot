173084794bf0849a375f8f89681f0bb5f9d8cf5c
DazzleOokami <dazzleookami@outlook.com>
2020-11-07 23:23:08 -0800

    Forgot to add other files, added them now. Along with an update to let the bot send error info through discord for debugging purposes.

--- cogs/emailsend.py
+++ cogs/emailsend.py
@@ -1,5 +1,6 @@
 # Import libs required for send_email function.
 import ssl
+import sys
 import email
 import smtplib
 from email.mime.text import MIMEText
@@ -7,7 +8,7 @@ from email.mime.multipart import MIMEMultipart
 
 
 # Function in charge of sending E-Mail.
-def send_email(esmtp: str, esend: str, erecv: str, esubj: str, epwrd: str, ebody: str, eport) -> None:
+async def send_email(esmtp: str, esend: str, erecv: str, esubj: str, epwrd: str, ebody: str, eport) -> None:
 
     # Encodes Message.
     msg = MIMEMultipart()
@@ -18,45 +19,51 @@ def send_email(esmtp: str, esend: str, erecv: str, esubj: str, epwrd: str, ebody
     txt = msg.as_string()
 
     # Creates SMTP session and starts TLS.
-    server = smtplib.SMTP(esmtp, eport)
-    server.starttls()
-    server.ehlo()
+    try:
+        server = smtplib.SMTP(esmtp, eport)
+        server.starttls()
+        server.ehlo()
 
     # Logs in and sends E-Mail.
-    server.login(esend, epwrd)
-    server.sendmail(esend, erecv, txt)
+        server.login(esend, epwrd)
+        server.sendmail(esend, erecv, txt)
+        sccs = "e-mail sent successfully"
+        return sccs
+    except:
+        emsg = f"ERROR: {sys.exc_info()}"
+        return emsg
 
 
-# Import discord.py library.
+    # Import discord.py library.
 from discord.ext import commands
-# Cog class for mail command.
-class mail_cmd(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
+class eml(commands.Cog):
+    def __init__(self, client):
+        self.bot = client
 
     @commands.command()
     async def mail(self, ctx):
-        async def ask(self, ctx):
+    #    async def ask(ctx):
+    #        def pred(m):
+    #            return m.author == ctx.author and m.channel == ctx.channel
+    #        ans = await ctx.wait_for('message', check=pred)
+    #        ans = ans.clean_content
+    #        return ans
+        async def askfunc(self, ctx, cont):
+            await ctx.channel.send(cont)
             def pred(m):
                 return m.author == ctx.author and m.channel == ctx.channel
-            ans = await ctx.wait_for('message', check=pred)
+            ans = await self.bot.wait_for('message', check=pred)
             ans = ans.clean_content
             return ans
-        await ctx.channel.send('SMTP:')
-        esmtp = ask(self, ctx)
-        await ctx.channel.send('Sender email:')
-        esend = ask(self, ctx)
-        await ctx.channel.send('receiver email:')
-        erecv = ask(self, ctx)
-        await ctx.channel.send('subject:')
-        esubj = ask(self, ctx)
-        await ctx.channel.send('email password:')
-        epwrd = ask(self, ctx)
-        await ctx.channel.send('email body:')
-        ebody = ask(self, ctx)
-        await ctx.channel.send('eport:')
-        eport = ask(self, ctx)
-        try:
-            send_email(esmtp, esend, erecv, esubj, epwrd, ebody, eport)
-        except ValueError:
-            ctx.channel.send('Sorry! There was an unexpected error sending your E-Mail: {0}'.format(ValueError))
+        esmtp = await askfunc(self, ctx, 'SMTP:')
+        esend = await askfunc(self, ctx, 'Sender e-mail:')
+        erecv = await askfunc(self, ctx, 'Receiver e-mail:')
+        esubj = await askfunc(self, ctx, 'Subject:')
+        epwrd = await askfunc(self, ctx, 'Password:')
+        ebody = await askfunc(self, ctx, 'e-mail body:')
+        eport = await askfunc(self, ctx, 'e-mail port')
+        ertrn = await send_email(esmtp, esend, erecv, esubj, epwrd, ebody, eport)
+        await ctx.channel.send(ertrn)
+
+def setup(bot):
+    bot.add_cog(eml(bot))
